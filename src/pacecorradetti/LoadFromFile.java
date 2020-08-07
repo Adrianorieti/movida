27dae7c7d2b9cac7861175d21f5ab7f0de3e4e0d
Andrea Corradetti <andreacorradetti28@gmail.com>
2020-08-07 13:04:09 +0200

    revised LoadFromFile.java

---
+++ src/pacecorradetti/LoadFromFile.java
@@ -0,0 +1,89 @@
+package pacecorradetti;
+
+import java.util.Scanner;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import movida.commons.*;
+
+public class LoadFromFile {
+	HashMap<String, Person> personMap;
+	HashMap<String, Movie> movieMap;
+	
+	public void load(File f) throws MovidaFileException, FileNotFoundException {
+
+		Scanner scan = new Scanner(f);
+
+		String line;
+		String title = null;
+		String directorName;
+		Person director;
+		int year = 0;
+		int votes = 0;
+		/* HashMap<String, Person> */personMap = new HashMap<String, Person>();
+		/* HashMap<String, Movie> */movieMap = new HashMap<String, Movie>();
+		List<Person> cast;
+		Person personToAdd = null;
+
+		while (scan.hasNextLine()) 
+		{
+			cast = new ArrayList<Person>();
+
+			line = scan.nextLine();		
+			title = formatLine(line);
+			
+			line = scan.nextLine();
+			year = Integer.parseInt(formatLine(line));
+			
+			line = scan.nextLine();
+			directorName = formatLine(line);	
+			director = new Person(directorName);
+			personMap.putIfAbsent(directorName, director);
+			
+			line = scan.nextLine();
+			String[] names = formatLine(line).split(",");
+
+			for (int i = 0; i < names.length; i++)  		// aggiunge all'array Person[] tutte le nuove persone, riempie il cast
+			{
+				// cast = addPerson(cast,new Person(names[i].trim())); //se fosse un array
+				personToAdd = new Person(names[i].trim());
+				cast.add(personToAdd); 
+				personMap.putIfAbsent(personToAdd.getName(), personToAdd);
+				
+			}
+
+			line = scan.nextLine();
+			votes = Integer.parseInt(formatLine(line));
+
+			Person[] castarray = cast.toArray(new Person[0]);
+			Movie movieToAdd = new Movie(title, year, votes, castarray, director);
+			movieMap.putIfAbsent(movieToAdd.getTitle(), movieToAdd);
+			
+			if (scan.hasNextLine())
+			{
+				scan.nextLine();				
+			}
+		}
+		scan.close();	
+	}
+
+	private String formatLine(String line) {
+		int index = line.indexOf(':');
+		line = line.substring(index + 1, line.length());
+		return line.trim();
+	}
+
+
+
+	/*
+	 * private static Person[] addPerson(Person[] persons, Person newperson) {
+	 * Person[] persons2 = new Person[persons.length + 1]; System.arraycopy(persons,
+	 * 0, persons2, 0, persons.length); persons2[persons2.length - 1] = newperson;
+	 * 
+	 * return persons2; }
+	 */
+}
--- src/pacecorradetti/Main.java
+++ src/pacecorradetti/Main.java
@@ -1,26 +1,31 @@
 package pacecorradetti;
 
+import java.io.File;
+import java.net.URL;
+import java.util.HashMap;
+
+import jdk.internal.reflect.ReflectionFactory.GetReflectionFactoryAction;
+import movida.commons.*;
+
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		
-		Integer arr[] = {6,5,4,3,2,8};
-		Algorithms.quickSort(arr);
-		
-		
+		LoadFromFile lff = new LoadFromFile();
+		lff.load( new File("C:\\Users\\Andrea\\eclipse-workspace\\Algos\\src\\movida\\commons\\esempio-formato-dati.txt"));
 
-	
-		
+		for(HashMap.Entry<String, Movie> entry : lff.movieMap.entrySet()) {
+		    String key = entry.getKey();
+		    Movie value = entry.getValue();
+		    
+		    System.out.println(
+		    		key + "\n" +
+		    		value.getCast().toString() + "\n" +
+		    		value.getYear().toString() + "\n" +
+		    		value.getDirector().getName());
 
-		
-		
-		
-		
-			
-			for (int a : arr) {
-				System.out.println(a);
-			}
-	
+		    // do what you have to do here
+		    // In your case, another loop.
+		}
 	}
 	
 }
