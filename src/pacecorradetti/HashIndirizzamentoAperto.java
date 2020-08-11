f54462597f97299191b768f5aded128f61ac02fc
Void <paceadrianolavoro@gmail.com>
2020-08-11 16:08:56 +0200

    Hash aggiornata

--- src/pacecorradetti/HashIndirizzamentoAperto.java
+++ src/pacecorradetti/HashIndirizzamentoAperto.java
@@ -1,36 +1,49 @@
 package pacecorradetti;
 
 
+public class HashIndirizzamentoAperto<K extends Comparable<K>,V extends Object> {
 
-public class HashIndirizzamentoAperto {
-
-	protected Object[] m;
+	protected class Pair {
+		K key;
+		V elem;
+		
+		Pair(K key, V elem) {
+			this.key = key;
+			this.elem = elem;
+		}
+	}  
+	protected Pair[] m;
 	private static Object deleted;
 	private int length = 0;
 	
-	public int getStringHash(String s,int l) {
-		int asciiValue = 0;
-		
-		for(int i = 0;i < s.length();i++) {
-			asciiValue = asciiValue + s.charAt(i);
-		}
-		return Math.abs(asciiValue % l);
-		
-	}
-	public int getStringHash2(String s,int l) {
-		int asciiValue = 0;
-		
-		for(int i = 0;i < s.length();i++) {
-			asciiValue = (int) (asciiValue + s.charAt(i) + 1);
-		}
-		return Math.abs(asciiValue % l);
-		
-	}
 	
-	public HashIndirizzamentoAperto(int l) {
+	
+//	public int getStringHash(K s,int l) {
+//		int asciiValue = 0;
+//		
+//		for(int i = 0;i < s.length();i++) {
+//			//asciiValue = asciiValue + s.charAt(i);
+//			
+//		}
+//		return Math.abs(asciiValue % l);
+//		
+//	}
+//	public int getStringHash2(String s,int l) {
+//		int asciiValue = 0;
+//		
+//		for(int i = 0;i < s.length();i++) {
+//			asciiValue = (int) (asciiValue + s.charAt(i) + 1);
+//		}
+//		return Math.abs(asciiValue % l);
+//		
+//	}
+//	
+	public HashIndirizzamentoAperto(int l)
+	{
 		this.length = l;
-		m = new Object[length];
-		for(int i =0;i < m.length;i++) {
+		m =   (Pair[]) new Object[length];
+		for(int i =0;i < m.length;i++) 
+		{
 			m[i] = null;
 		}
 		this.deleted = new Object();
@@ -40,84 +53,77 @@ public class HashIndirizzamentoAperto {
 		for(int i =0; i< m.length;i++)
 		{
 			if(m[i] == null) System.out.print(i + " ");
-			else if(m[i].equals(deleted)) System.out.print("deleted");
-			else System.out.print(m[i].getClass().getName() + " ");
+			else if(m[i].elem.equals(deleted)) System.out.print("deleted");
+			else System.out.print(m[i].key);
 		}
 	}
 	
-	public Object search(Object o,String name,int l) throws MovidaKeyException {
-		int key = getStringHash(name, l);
-		int key2 = getStringHash2(name,l);
-		if(m[key].equals(o)) return m[key];
-		else if(m[key] == null) return null;
+	public V search(K key) throws MovidaKeyException
+	{
+		int hash = key.hashCode() % length;
+		if(m[hash].key.equals(key)) return (m[hash].elem);
+		else if(m[hash] == null) return null;
 		else {
-			for(int i=0;i < m.length;i++) {
-				if((m[key + (i * key2) % l].equals(o))) 
-					return m[(key + (i * key2) % l)];
+			for(int i=0;i < m.length;i++) 
+			{
+				int hash2 = hash + (i * (key.hashCode() + 1)) % length ;
+				if(m[hash2].key.equals(key)) 
+					return m[hash2].elem;
 			}
 		}
 		throw new MovidaKeyException();
 	}
 	
-	public void delete(Object o,String name,int l) throws MovidaKeyException
+	public void delete(K key) throws MovidaKeyException
 	{
-		int key = getStringHash(name, l);
-		int key2 = getStringHash(name, l);
-		if(m[key].equals(o))
+		int hash = key.hashCode() % length;
+		if(m[hash].key.equals(key))
 		{ 
-			m[key] = deleted;
+			m[hash].elem = (V) deleted;
 		}
-		else if(m[key] == null) throw new MovidaKeyException();
-		else if(m[key] == deleted)
+		else if(m[hash] == null) throw new MovidaKeyException();
+		else if(m[hash].elem == deleted)
 		{
-			for(int i=0;i < m.length;i++) {
-				if((m[key + (i * key2) % l].equals(o)))
+			for(int i=0;i < m.length;i++) 
+			{
+				int hash2 = hash + (i * (key.hashCode() + 1)) % length ;
+				if((m[hash2].key.equals(key)))
 				{
-					m[key + (i * key2) % l] = deleted;
+					m[hash2].elem = (V) deleted;
 				}
 		}
 		}
 	}
-	// returns -1 if the array is full
-	public int insert(Object o,String s,int l) {
-		int key = getStringHash(s,l);
-		int key2 = getStringHash2(s,l);
-		if((m[key] == null) || (m[key] == deleted)) {
-			m[key] = o ;
-		return (key);
+	
+	public void insert(K key,V value) throws MovidaBoundException
+	{
+		int hash = key.hashCode() % length;
+		boolean inserted = false;
+		if((m[hash] == null) || (m[hash] == deleted))
+		{
+			m[hash] = new Pair(key,value) ;
+			inserted = true;
 		}
-		else if(m[key] == o) {
-			m[key] = o;
-			return(key);
+		else if(m[hash].key == key)
+		{
+			m[hash].elem = value; // sovrascrive l'oggetto
+			inserted = true;
 		}
-		else  {
-			for(int i=0;i < m.length;i++) {
-				if((m[key + (i * key2) % l] == null) || ((m[key + (i * key2) % l] == deleted)))
+		else 
+		{
+			for(int i=0;i < m.length;i++)
+			{
+				int hash2 = hash + (i * (key.hashCode() + 1)) % length ;
+				if((m[hash2] == null) || ((m[hash2] == deleted)))
 				{
-					m[key + (i * key2) % l] = o;
-					return(key + (i * key2) % l);
+					m[hash2] = new Pair(key,value);
+					inserted = true;
+					break;
 				}
 			}
+			
 		}
-			/*boolean found = false;
-			for(int i =key +1;i < m.length;i++) {
-				if(m[i] == null) 
-				{
-					m[i] = o;
-				found = true;
-				return(i);
-				}
-			}
-			if(!found) {
-				for(int i =0;i<key;i++)
-				{
-					if(m[i]== null) m[i] = o;
-					return(i);
-				}
-			}
-		}*/
-		return -1;
-		
+		if(!inserted) throw new MovidaBoundException();
 	}
 
 }
