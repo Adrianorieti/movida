00dcfe2c76073d5b378f75df8992a7bd9982008e
Void <paceadrianolavoro@gmail.com>
2020-08-25 18:55:17 +0200

    remastered

--- src/pacecorradetti/loadConfig.java
+++ src/pacecorradetti/loadConfig.java
@@ -2,6 +2,8 @@ package pacecorradetti;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 import movida.commons.MapImplementation;
@@ -15,19 +17,11 @@ public class loadConfig {
 	String line = null;
 
 	public loadConfig(File f) throws MovidaFileException, FileNotFoundException {
-		
-		
-
 		Scanner scan = new Scanner(f);
-		while (scan.hasNextLine()) 
-		{
-			
 			line = scan.nextLine();
 			Algorithm = formatLine(line);
 			line = scan.nextLine();
 			Map = formatLine(line);
-			
-		}
 		scan.close();
 	}
 	
@@ -59,6 +53,30 @@ public class loadConfig {
 		}
 		else
 			throw new MovidaFileException();
+	}
+	
+	public void overrideAlg(File file,String algo) throws IOException {
+		Scanner scan = new Scanner(file);
+			line = scan.nextLine();
+			line = scan.nextLine();
+			Map = formatLine(line);
+		scan.close();
+		FileWriter write = new FileWriter(file);
+		write.append("sorting_algorithm=" + algo +"\n");
+		write.append("map_implementation=" + Map);
+		write.close();
+		
+	}
+	public void overrideMap(File file,String map) throws IOException {
+		Scanner scan = new Scanner(file);
+		line = scan.nextLine();
+		Algorithm = formatLine(line);
+		scan.close();
+		FileWriter write = new FileWriter(file);
+		write.append("sorting_algorithm=" + Algorithm +"\n");
+		write.append("map_implementation=" + map);
+		write.close();
+		
 	}
 	
 	protected String formatLine(String line) {
