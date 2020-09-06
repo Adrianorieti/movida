148d0329c19ef43d705e281b0b163b3aa59b53ea
Void <paceadrianolavoro@gmail.com>
2020-09-06 16:09:10 +0200

    Comments added

--- src/pacecorradetti/Map.java
+++ src/pacecorradetti/Map.java
@@ -3,8 +3,17 @@ package pacecorradetti;
 import java.util.ArrayList;
 import java.util.Set;
 
+
+/**
+ * Definisce una classe astratta utilizzata dalle strutture dati
+ */
 abstract class Map<K extends Comparable<K>, V extends Object> {
 	
+	/**Definisce una classe con due valori : @key la chiave assegnata all'elemento per la ricerca  @value l'elemento stesso 
+	 * 
+	 * @author pacecorradetti
+	 *
+	 */
 	final class Entry  {
 		protected K key;
 		protected V value;
@@ -23,27 +32,57 @@ abstract class Map<K extends Comparable<K>, V extends Object> {
 		}
 	}
 	
-	
+	/**
+	 * Inserisce un dato nella struttura dati sovrascrivendolo se già presente
+	 * @param key la stringa con la quale effettuare la ricerca
+	 * @param value l'oggetto che verrà inserito
+	 */
 	abstract public void put(K key, V value);
 
-	
+	/**
+	 * Cancella l'elemento corrispondete al
+	 * @param key
+	 */
 	abstract void delete(K key) throws MovidaKeyException;
 	
-	
+	/**
+	 * Effettua una ricerca dell'elemento corrispondente al
+	 * @param key
+	 * @return <V> Object
+	 */
 	abstract public V search(K key) throws MovidaKeyException;
 
-	
+	/**
+	 * Imposta a null ogni area di memoria della struttura dati
+	 */
 	abstract public void clear();
 	
+	/**
+	 * Raccoglie in un insieme tutti gli Entry contenuti in una struttura dati
+	 * @return Set di Entry
+	 */
 	
 	abstract public Set<Entry> entrySet();
 	
+	/**
+	 * Raccoglie in un ArrayList tutti gli elementi di una struttura dati
+	 * @return ArrayList di <V> Object
+	 */
 	
 	abstract public ArrayList<V> valueList();
 
+	/**
+	 * Inserisce un elemento nella struttura dati solamente se non già presente
+	 * @param key la stringa per la ricerca nella struttura
+	 * @param value l'elemento che và inserito
+	 */
 
 	public abstract void putIfAbsent(K key, V value);
 
+	/**
+	 * Ritorna il size della struttura dati
+	 * @return int 
+	 */
 	
 	abstract public int size();
 	
--- src/pacecorradetti/PersonRole.java
+++ src/pacecorradetti/PersonRole.java
@@ -1,5 +1,10 @@
 package pacecorradetti;
 
+/**
+ * Ruolo che un Object Person può ricoprire all'interno di un Object Movie
+ * @author pacecorradetti
+ *
+ */
 public enum PersonRole {
 	actor,
 	director
